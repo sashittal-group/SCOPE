7a022461f2186f310141a00aedcfa0dc7364c9cd
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2025-10-08 02:16:05 -0400

    Use old code to get clone tree

--- src/phylogeny_utils.py
+++ src/phylogeny_utils.py
@@ -31,3 +31,36 @@ def generate_perfect_phylogeny(df_binary):
         solT_cell.add_edge(curr_node, cell_id)   
 
     return solT_mut, solT_cell
+
+
+
+def build_phylogeny(B):
+    G = nx.DiGraph()
+    root = "root"
+    G.add_node(root, label="root")
+
+    taxa_sets = []
+    for cell_id, row in B.iterrows():
+        if not row.any():
+            continue
+        chars = frozenset([j for j, val in enumerate(row) if val == 1])
+        taxa_sets.append((cell_id, chars))
+
+    sorted_cells_with_taxa_sets = sorted(taxa_sets, key=lambda x: len(x[1]))
+    node_map = {frozenset(): root}
+
+    for cell_id, taxa_set in sorted_cells_with_taxa_sets:
+        parent = max([p for p in node_map if p.issubset(taxa_set)], key=len)
+        parent_cell = node_map[parent]
+
+        diff = taxa_set - parent  # mutations gained at this node
+        mut_diffs = [B.columns[j] for j in sorted(diff)]
+        edge_name = "\n".join(str(mut_diffs)) if mut_diffs else "empty"
+        node_name = cell_id
+
+        G.add_node(node_name, label=node_name)
+        G.add_edge(parent_cell, node_name, label=edge_name)
+
+        node_map[taxa_set] = node_name
+
+    return G
